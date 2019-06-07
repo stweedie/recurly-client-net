d1a11e1129b2a73f76c9ad0f20610fa1acce41e4
Benjamin Eckel <bhelx@simst.im>
2019-06-06 22:21:00 -0500

    Thu Jun  6 22:21:00 CDT 2019 Upgrade API version v2017-09-30

--- Recurly/Resources/AccountCreate.cs
+++ Recurly/Resources/AccountCreate.cs
@@ -9,61 +9,63 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json;
 
-namespace Recurly.Resources {
-  [ExcludeFromCodeCoverage] 
-  public class AccountCreate : Request {
-  
-    
-    [JsonProperty("acquisition")]
-    public AccountAcquisitionUpdatable Acquisition { get; set; }
-  
-    
-    [JsonProperty("address")]
-    public Address Address { get; set; }
-  
-    
-    [JsonProperty("billing_info")]
-    public BillingInfoCreate BillingInfo { get; set; }
-  
-    /// <value>Additional email address that should receive account correspondence. These should be separated only by commas. These CC emails will receive all emails that the `email` field also receives.</value>
-    [JsonProperty("cc_emails")]
-    public string CcEmails { get; set; }
-  
-    /// <value>The unique identifier of the account. This cannot be changed once the account is created.</value>
-    [JsonProperty("code")]
-    public string Code { get; set; }
-  
-    
-    [JsonProperty("company")]
-    public string Company { get; set; }
-  
-    /// <value>The email address used for communicating with this customer. The customer will also use this email address to log into your hosted account management pages. This value does not need to be unique.</value>
-    [JsonProperty("email")]
-    public string Email { get; set; }
-  
-    
-    [JsonProperty("first_name")]
-    public string FirstName { get; set; }
-  
-    
-    [JsonProperty("last_name")]
-    public string LastName { get; set; }
-  
-    
-    [JsonProperty("shipping_addresses")]
-    public List<ShippingAddressCreate> ShippingAddresses { get; set; }
-  
-    /// <value>The tax status of the account. `true` exempts tax on the account, `false` applies tax on the account.</value>
-    [JsonProperty("tax_exempt")]
-    public bool? TaxExempt { get; set; }
-  
-    /// <value>A secondary value for the account.</value>
-    [JsonProperty("username")]
-    public string Username { get; set; }
-  
-    /// <value>The VAT number of the account (to avoid having the VAT applied). This is only used for manually collected invoices.</value>
-    [JsonProperty("vat_number")]
-    public string VatNumber { get; set; }
-  
-  }
+namespace Recurly.Resources
+{
+    [ExcludeFromCodeCoverage]
+    public class AccountCreate : Request
+    {
+
+
+        [JsonProperty("acquisition")]
+        public AccountAcquisitionUpdatable Acquisition { get; set; }
+
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+
+
+        [JsonProperty("billing_info")]
+        public BillingInfoCreate BillingInfo { get; set; }
+
+        /// <value>Additional email address that should receive account correspondence. These should be separated only by commas. These CC emails will receive all emails that the `email` field also receives.</value>
+        [JsonProperty("cc_emails")]
+        public string CcEmails { get; set; }
+
+        /// <value>The unique identifier of the account. This cannot be changed once the account is created.</value>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+
+        [JsonProperty("company")]
+        public string Company { get; set; }
+
+        /// <value>The email address used for communicating with this customer. The customer will also use this email address to log into your hosted account management pages. This value does not need to be unique.</value>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+
+        [JsonProperty("shipping_addresses")]
+        public List<ShippingAddressCreate> ShippingAddresses { get; set; }
+
+        /// <value>The tax status of the account. `true` exempts tax on the account, `false` applies tax on the account.</value>
+        [JsonProperty("tax_exempt")]
+        public bool? TaxExempt { get; set; }
+
+        /// <value>A secondary value for the account.</value>
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        /// <value>The VAT number of the account (to avoid having the VAT applied). This is only used for manually collected invoices.</value>
+        [JsonProperty("vat_number")]
+        public string VatNumber { get; set; }
+
+    }
 }
